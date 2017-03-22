bbfe7b3c57e40eae0411aa7c626ffcc2dd820f04
Michael J. Bennett <michael.bennett@rangle.io>
2017-03-22 12:28:31 -0400

    Adds backend TS system

---
+++ backend/ts/blog-list.spec.ts
@@ -0,0 +1,12 @@
+import { handler } from './blog-list';
+const expect = require('chai').expect;
+
+describe('Blog List Endpoint', () => {
+  it('should callback successfully', (done) => {
+    handler(null, null, (err, result) => {
+      expect(err).to.equal(null);
+      expect(result).to.equal('victory');
+      done();
+    });
+  });
+});
---
+++ backend/ts/blog-list.ts
@@ -0,0 +1,3 @@
+export function handler(event: any, context: any, callback: Function) {
+  callback(null, 'victory');
+}
