979958c5aa281627af1b9cbe40a9d3ddf240ddd2
Michael J. Bennett <michael.bennett@rangle.io>
2017-03-22 16:49:11 -0400

    Can now list from s3 bucket

--- backend/ts/blog-list.ts
+++ backend/ts/blog-list.ts
@@ -1,20 +1,37 @@
+import * as AWS from 'aws-sdk';
 import { cors } from './cors';
+import { s3Factory } from './s3';
 
-export function handler(event: any, context: any, callback: Function) {
-  callback(null, cors(200, [
-    {
-      id: 'entropy-things-test-1',
-      title: 'Test 1',
-      body: 'The quick brown fox...',
-      date: Date.now(),
-      author: 'Somebody'
-    },
-    {
-      id: 'entropy-things-test-2',
-      title: 'Test 2',
-      body: 'Lorem Ipsump',
-      date: Date.now(),
-      author: 'Nobody'
-    }
-  ]));
-}
+const S3 = new AWS.S3();
+const s3 = s3Factory(S3, 'simple-blogs-bucket');
+
+export function handler(event, context, callback) {
+  const token = event.token ? event.token : undefined;
+
+  try {
+    s3.list(token, (err, result) => {
+      if (err) {
+        console.log('error', err.message);
+        callback(null, cors(500, { error: err.message }));
+        return;
+      }
+      console.log('Success');
+      if (result.keys && result.keys.length) {
+        callback(null, cors(200, result));
+      } else {
+        callback(null, cors(200, [ {
+          keys: {
+            id: 'genesis-blog-' + Date.now().toString(16),
+            title: 'Welcome to this blog platform',
+            author: 'Open Lane',
+            date: Date.now(),
+          },
+          token: undefined,
+        }]));
+      }
+    });
+  } catch (err) {
+    console.log('fail', err.message);
+    callback(null, cors(500, { error: err.message }));
+  }
+};
